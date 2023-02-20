43c18f182166ebf90c3113559060a99b605ac7f2
Mazen Abdul <mazenabdul98@gmail.com>
2023-02-19 18:02:02 -0600

    fix(GHA): add docker-compose for prod AWS EB deployments

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Test Mazen 200</p>
+        <p>Test Mazen 100</p>
         <a
           className="App-link"
           href="https://reactjs.org"
